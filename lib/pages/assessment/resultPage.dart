361f9271dbe10c3208aa4e6ef021f6dc0273aa04
nutta <nutta.nut2327@gmail.com>
2026-02-09 18:11:47 +0700

    Update assessment page UI

--- lib/pages/assessment/resultPage.dart
+++ lib/pages/assessment/resultPage.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:fe/routes/app_routes.dart';
 import 'package:fe/widgets/mainButton.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class ResultPage extends StatelessWidget {
   final int totalScore;
@@ -13,25 +14,62 @@ class ResultPage extends StatelessWidget {
 
   String get resultMessage {
     if (totalScore <= 4) {
-      return 'Having a low level of stress.';
+      return 'Your stress level is low.';
     } else if (totalScore <= 7) {
-      return 'Having a moderate level of stress.';
+      return 'Your stress level is moderate.';
     } else if (totalScore <= 9) {
-      return 'Having a high level of stress.';
+      return 'Your stress level is high.';
     } else {
-      return 'Having a severe level of stress.';
+      return 'Your stress level is severe.';
     }
   }
 
   String get detailResult {
     if (totalScore <= 4) {
-      return 'You have a low level of stress, which tends to be short-term.';
-    } else if (totalScore <= 7) {
-      return 'You have a moderate level of stress.';
-    } else if (totalScore <= 9) {
-      return 'You have a high level of stress.';
-    } else {
-      return 'You have a severe level of stress.';
+      return
+          'Congratulations!\n\n'
+          'Your stress level is within a normal range.\n'
+          'This is everyday stress that you can manage\n'
+          'on your own and it does not affect your health.\n\n'
+          'You have enough energy to take care of yourself\n'
+          'and support people around you.\n'
+          'Keep maintaining this healthy balance.';
+    } 
+    else if (totalScore <= 7) {
+      return
+          'Your stress level is moderate.\n\n'
+          'This usually comes from daily responsibilities\n'
+          'and life challenges.\n'
+          'It is considered normal and not harmful.\n\n'
+          'This level of stress can even help motivate you\n'
+          'to stay alert and handle situations effectively.';
+    } 
+    else if (totalScore <= 9) {
+      return
+          'Your stress level is high.\n\n'
+          'You may experience strong emotional reactions\n'
+          'during this period, but they often improve\n'
+          'once the situation gets better.\n\n'
+          'Try simple self-care such as:\n'
+          '• Deep breathing\n'
+          '• Getting enough rest\n'
+          '• Talking with someone you trust\n'
+          '• Doing relaxing or spiritual activities\n\n'
+          'If you do not feel better after 2 weeks,\n'
+          'it is recommended to consult a doctor\n'
+          'or a mental health professional.';
+    } 
+    else {
+      return
+        'Your stress level is severe and may affect both\n'
+        'your physical and mental health.\n\n'
+        'There is a risk of anxiety, depression,\n'
+        'or thoughts of self-harm.\n\n'
+        'It is strongly recommended that you see a doctor\n'
+        'or mental health professional as soon as possible\n'
+        'for close and continuous care over the next\n'
+        '3–6 months.\n\n'
+        'You do not have to face this alone.\n';
     }
   }
 
@@ -41,6 +79,16 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Assessment Results'),
         centerTitle: true,
+        leading: IconButton(
+          icon: HugeIcon(
+            icon: HugeIcons.strokeRoundedArrowLeft01,
+            size: 24,
+            strokeWidth: 2,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, AppRoutes.bottomnavbar);
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(24),
@@ -56,13 +104,13 @@ class ResultPage extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
-            SizedBox(height: 24),
+            SizedBox(height: 16),
             Text(
               '$totalScore',
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
               ),
             ),
             SizedBox(height: 24),
@@ -70,18 +118,19 @@ class ResultPage extends StatelessWidget {
               resultMessage,
               textAlign: TextAlign.center,
               style: TextStyle(
-                fontSize: 18, 
-                fontWeight: FontWeight.w500
+                fontSize: 24, 
+                fontWeight: FontWeight.w700
               ),
             ),
-            SizedBox(height: 24),
+            SizedBox(height: 32),
             Text(
               detailResult,
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: 14),
             ),
-            SizedBox(height: 32),
-            ElevatedButton(
+            SizedBox(height: 48),
+            MainButton(
+              text: 'Back Home',
               onPressed: () {
                 Navigator.pushNamedAndRemoveUntil(
                   context,
@@ -89,7 +138,6 @@ class ResultPage extends StatelessWidget {
                   (route) => false,
                 );
               },
-              child: const Text('Back Home'),
             ),
           ],
         ),
---
+++ lib/widgets/secondButton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SecondButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const SecondButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(21911200),
+        ),
+        backgroundColor: Color(0xFFFEF8FC),
+        foregroundColor: Color(0xFFF6E8F5),
+        side: const BorderSide(color: Color(0xFFF6E8F5), width: 1.5),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Color( 0xFF4A4458),
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+}
