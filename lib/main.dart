9c4568aedc0a25719bba1033525c5b56b8006139
YuukaKawai <117619748+yuuka2803@users.noreply.github.com>
2026-03-15 22:05:55 +0700

    feat: add network logger (#26)

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:fe/services/fcm_notification_service.dart';
+import 'package:cote_network_logger/cote_network_logger.dart';
 import 'package:fe/firebase_options.dart';
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await startNetworkLogServer();
 
   await dotenv.load(fileName: ".env");
 
